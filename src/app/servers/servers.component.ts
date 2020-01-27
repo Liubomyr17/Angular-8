11008b607c57ad404f0d34734f7299052ff9097f
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-01-27 14:35:22 +0200

    24.nfFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ allowNewServer = false;
 serverCreationStatus = 'No server was created!';
 serverName = 'Testserver';
 serverCreated = false;
+servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
   setTimeout(() => {
@@ -27,6 +28,7 @@ serverCreated = false;
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is' + this.serverName; 
   }
 
