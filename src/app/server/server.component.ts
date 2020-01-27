329e1a00feea682269b4306125220a8a1193be7d
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-01-27 14:20:27 +0200

    22.ngstyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,17 @@ export class ServerComponent {
 serverId: number = 10;
 serverStatus: string = 'offLine';
 
+constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offLine';
+
+}
+
 getServerStatus() {
     return this.serverStatus;
 }
+
+getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+}
+
 }
